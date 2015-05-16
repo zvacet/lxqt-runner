3157ecd0feec2c710e830090aa877e9ed88a4f3f
zvacet <ikoli@yahoo.com>
2015-05-16 07:20:40 +0200

    Create lxqt-runner_hr.ts

---
+++ translations/lxqt-runner_hr.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="14"/>
+        <source>Runner Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="20"/>
+        <source>Appearance</source>
+        <translation type="unfinished">Izgled</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="26"/>
+        <source>Positioning:</source>
+        <translation type="unfinished">Pozicioniranje:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="36"/>
+        <source>Show on:</source>
+        <translation type="unfinished">Pokaži na:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="56"/>
+        <source>Shortcut:</source>
+        <translation type="unfinished">Prečac:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="60"/>
+        <source>Top edge of the screen</source>
+        <translation type="unfinished">Vrh ruba zaslona</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="61"/>
+        <source>Center of the screen</source>
+        <translation type="unfinished">Sredina zaslona</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="67"/>
+        <source>Focused screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="71"/>
+        <source>Always on screen %1</source>
+        <translation type="unfinished">Uvijek na zaslonu %1</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="80"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="26"/>
+        <source>Application launcher </source>
+        <translation type="unfinished">Pokretač programa</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="92"/>
+        <source>Configure</source>
+        <translation type="unfinished">Konfiguriraj</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="96"/>
+        <source>Clear History</source>
+        <translation type="unfinished">Očisti povijest</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="337"/>
+        <source>Show/hide runner dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../providers.cpp" line="388"/>
+        <source>History</source>
+        <translation type="unfinished">Povijest</translation>
+    </message>
+    <message>
+        <location filename="../providers.cpp" line="779"/>
+        <source>Mathematics</source>
+        <translation type="unfinished">Matematika</translation>
+    </message>
+    <message>
+        <location filename="../providers.cpp" line="840"/>
+        <source>Power Management</source>
+        <translation type="unfinished">Upravljanje energijom</translation>
+    </message>
+</context>
+</TS>
